74590596178e612db1dcec088341dd7761d18bab
prestf8 <prestf8@gmail.com>
2023-01-06 10:26:18 -0800

    base components of project complete

--- src/components/CurriculumVitae.js
+++ src/components/CurriculumVitae.js
@@ -14,6 +14,41 @@ function CurriculumVitae(props) {
                 <h4 className="header-component phone-number-display">{phoneNumber}</h4>
             </header>
             <div className="cv-main">
+                <div>
+                    <h4 className="cv-main-subtitle">Experience</h4>
+                    {workExperience.map((experience) => {
+                        return (
+                        <div key={experience.id} className="subcomponent">
+                            <div className="subcomponent-left">
+                                <p className="time-range"><span>{experience.weFrom}</span> to <span>{experience.weTo}</span></p>
+                            </div>
+                            
+                            <div className="subcomponent-right">
+                                <h6 className="company-name">{experience.companyName},&nbsp;</h6>
+                                <p>{experience.title},&nbsp;</p>
+                                <p>{experience.description}&nbsp;</p>
+                            </div>
+
+                        </div>)
+                    })}
+                </div>
+                <div>
+                    <h4 className="cv-main-subtitle">Education</h4>
+                    {education.map((educationComponent) => {
+                        return (
+                        <div key={educationComponent.id} className="subcomponent">
+                            <div className="subcomponent-left">
+                                <p className="time-range"><span>{educationComponent.eduFrom}</span> to <span>{educationComponent.eduTo}</span></p>
+                            </div>
+
+                            <div className="subcomponent-right">
+                                <h6 className="school">{educationComponent.school},&nbsp;</h6>
+                                <p>{educationComponent.titleOfStudy}</p>
+                            </div>
+
+                        </div>)
+                    })}
+                </div>
 
             </div>
         </div>
